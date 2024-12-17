b7e8787c2def63d0bf02bda26756a96903ab1a8b
Deepak Wadhwani <66224613+imdeepakw@users.noreply.github.com.>
2024-12-16 23:14:09 -0500

    added cors

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', getWrestler)
 async function getWrestler(){
     let wrestlerName = document.querySelector('input').value
 
-    const res = await fetch('/api/' + wrestlerName)
+    const res = await fetch('https://wrestling-api-app.onrender.com/api/' + wrestlerName)
     const data = await res.json()
 
     console.log(data)
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 let wrestlers = {
     "rey mysterio": {
         "name": "Oscar Gutierrez Rubio",
